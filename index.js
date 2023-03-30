a0d999209340bb314334b7664ccc3fc856b74b8c
EnZon3 <37979038+EnZon3@users.noreply.github.com>
2023-03-29 20:31:39 -0400

    going to math subject works now

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cookies = require('./cookies.json');
 const fs = require('fs');
 
-async function test() {
+async function reading() {
   const Nullify = path.join(process.cwd(), 'Nullify');
   const browser = await puppeteer.launch({
     headless: 'new',
@@ -25,7 +25,7 @@ async function test() {
   await page.goto('https://login.i-ready.com/student/dashboard/home', { waitUntil: 'networkidle0' });
   console.log('I-Ready loaded, now going to Reading subject...');
 
-  let reading = await page.$x("/html/body/div/div[1]/section/div/div[2]/main/div/div/div/div/div/div[1]/span/div/button")
+  let reading = await page.$x("/html/body/div[1]/div[1]/section/div/div[2]/main/div/div/div/div/div/div[1]/span/div/button")
   await reading[0].click({ waitUntil: 'domcontentloaded'})
 
   await page.waitForTimeout(4000)
@@ -37,4 +37,47 @@ async function test() {
   await browser.close();
 };
 
-test();
+async function math() {
+  const Nullify = path.join(process.cwd(), 'Nullify');
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: [
+      `--disable-extensions-except=${Nullify}`,
+      `--load-extension=${Nullify}`
+    ],
+    executablePath: executablePath(),
+  });
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+
+  console.log('setting cookies...');
+  await page.setCookie(...cookies);
+
+  console.log('loading i-ready...')
+  await page.goto('https://login.i-ready.com/student/dashboard/home', { waitUntil: 'networkidle0' });
+  console.log('I-Ready loaded, now going to Math subject...');
+
+  let math = await page.$x("/html/body/div[1]/div[1]/section/div/div[2]/main/div/div/div/div/div/div[2]/span/div/button")
+  await math[0].click({ waitUntil: 'domcontentloaded'})
+
+  await page.waitForTimeout(4000)
+
+  console.log('taking screenshot...');
+  await page.screenshot({
+    path: 'screenshot.jpg'
+  });
+  await browser.close();
+};
+
+
+//get args
+let subject = process.argv[2];
+
+//check if reading or math
+if (subject == 'm' || subject == 'math') {
+  math()
+  return;
+} else {
+  reading()
+  return;
+}
